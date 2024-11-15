800af858a70bfdf0093197910f1f377e850ac86f
umasriurimella <umasriurimella@gmail.com>
2024-11-16 01:17:14 +0530

    eleven

---
+++ facesearchai/frontend/src/Guest.jsx
@@ -0,0 +1,95 @@
+import {useRef,useEffect} from 'react'
+// import './face.css'
+import { Link, NavLink } from 'react-router-dom'
+ import './Guest.css'
+ import * as faceapi from 'face-api.js'
+
+function App(){
+  const videoRef = useRef()
+  const canvasRef = useRef()
+
+  // LOAD FROM USEEFFECT
+  useEffect(()=>{
+    startVideo()
+    videoRef && loadModels()
+
+  },[])
+
+
+
+  // OPEN YOU FACE WEBCAM
+  const startVideo = ()=>{
+    navigator.mediaDevices.getUserMedia({video:true})
+    .then((currentStream)=>{
+      videoRef.current.srcObject = currentStream
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+  // LOAD MODELS FROM FACE API
+
+  const loadModels = ()=>{
+    Promise.all([
+      // THIS FOR FACE DETECT AND LOAD FROM YOU PUBLIC/MODELS DIRECTORY
+      faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+      faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+      faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+      faceapi.nets.faceExpressionNet.loadFromUri("/models")
+
+      ]).then(()=>{
+      faceMyDetect()
+    })
+  }
+
+  const faceMyDetect = ()=>{
+    setInterval(async()=>{
+      const detections = await faceapi.detectAllFaces(videoRef.current,
+        new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+
+      // DRAW YOU FACE IN WEBCAM
+      canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(videoRef.current)
+      faceapi.matchDimensions(canvasRef.current,{
+        width:940,
+        height:650
+      })
+
+      const resized = faceapi.resizeResults(detections,{
+         width:940,
+        height:650
+      })
+
+      faceapi.draw.drawDetections(canvasRef.current,resized)
+      faceapi.draw.drawFaceLandmarks(canvasRef.current,resized)
+      faceapi.draw.drawFaceExpressions(canvasRef.current,resized)
+
+
+    },1000)
+  }
+
+  return (
+    <div className="myapp">
+    <h1>FACE DETECTION</h1>
+    <h2>As of now i have added the face detection feature it shows what 
+      expression our face gives happy or sad</h2>
+      <h2>
+      (scroll down and align your face properly to camera)
+    </h2>
+    <h2>what to unlock more features Buy plan</h2>
+    <NavLink to='/payment' className='flex flex-col items-center gap-1'>
+            <p>PAYMENT</p>
+            </NavLink>
+               
+
+      <div className="appvide">
+        
+      <video crossOrigin="anonymous" ref={videoRef} autoPlay></video>
+      </div>
+      <canvas ref={canvasRef} width="940" height="650"
+      className="appcanvas"/>
+    </div>
+    )
+
+}
+
+export default App;
