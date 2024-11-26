15c347ceb7a553ae24b8a8547a51e98e3b26e10b
umasriurimella <umasriurimella@gmail.com>
2024-11-26 08:47:32 +0530

    c

--- facesearchai/backend/server.js
+++ facesearchai/backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import connectDB from './config/mongodb.js'
+// import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
 import Stripe from 'stripe'
